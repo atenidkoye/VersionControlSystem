dc62bec3e7ac68a3bb73cd514896a2cc1a7370a9
atenidkoye <98021081+atenidkoye@users.noreply.github.com>
2023-09-29 12:04:24 +0100

    program completed.

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,28 @@ public class Main {
         int Tries = 0;
         while (true) {
             Tries++;
-            Ans = in.nextLine();
+            Name = "mary";
             System.out.println("please, guess my name");
-            if (Name.equals(Ans)) {
+            Ans = in.nextLine();
+            if (Ans.equals(Name)) {
                 System.out.println("congratulations!");
+                System.out.println("guess: "+Tries);
                 break;
+            }
+            else if (Ans.equals("y")) {
+                System.out.println("thank you for playing");
+                System.out.println("guess: "+Tries);
+                break;
+
             } else if (Ans.equals("n")) {
                 System.out.println("thank you for playing");
                 continue;
+            }
+            else {
+                System.out.println("Try Again");
+                System.out.println("do you want to quite? (y/n)");
+//                System.out.println("guesses " + Tries--);
 
-            } else if (Ans.equals("y")) {
-                System.out.println("thank you for playing");
-                break;
 
             }
 
